cf66a327f70603fea56c6155fe1f837a98363d9c
Pranali Waghmare <126958148+Pranali2624@users.noreply.github.com>
2024-05-05 21:06:38 +0530

    Add files via upload

---
+++ AD.php
@@ -0,0 +1,14 @@
+<?php 
+$A=array("Pranali","Apurva","Aniket","Pranav"); 
+foreach($A as $P){ echo" $P <br>"; } 
+echo"<br><br>"; 
+echo " Array In Acending Order <br> <br>"; 
+sort($A); 
+foreach($A as $P){ echo" $P <br>"; } 
+echo"<br><br>"; 
+echo " Array In Decending Order <br> <br>"; 
+rsort($A); 
+foreach($A as $P){ 
+ echo" $P <br>"; }?> 
+
+
