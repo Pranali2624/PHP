02a3f10b0a54d6cf39a4df512015a1698b77972b
Pranali Waghmare <126958148+Pranali2624@users.noreply.github.com>
2024-05-14 22:02:17 +0530

    Add files via upload

---
+++ request.php
@@ -0,0 +1,11 @@
+
+<html>
+<body>
+<form action="r.php"
+method="REQUEST">
+name:<input type ="text" name="name"><br>
+email:<input type ="text"
+name="email"><br>
+<input type="submit" name="send"></form>
+</body>
+</html>
